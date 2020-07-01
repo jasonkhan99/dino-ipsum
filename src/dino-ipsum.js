fdb62d4d0ce585c8930571c03b789882ddcdc6f2
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-01 11:39:36 -0700

    create function for api call and basic ui
    Taylor Somers <tsomers@yandex.com>
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- src/dino-ipsum.js
+++ src/dino-ipsum.js
@@ -1,2 +1,16 @@
-// project business logic goes here
-// exports!
+export class DinoIpsumGenerator {
+  async getDinoNames() {
+    try {
+      let response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse = false;
+    } catch(error) {
+      return false;
+    }
+  }
+}
