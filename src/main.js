f508e7162b6c9ae9c083d49ecaa8ff9effe08f29
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-02 14:55:24 -0700

    manipulate arrays in submit function
    co-authored-by: Cody Fritz <clanalia55@gmail.com>
    Co-authored-by: Taylor Somers <tsomers@yandex.com>
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- src/main.js
+++ src/main.js
@@ -8,81 +8,56 @@ import './css/custom.css';
 import './css/styles.css';
 import { DinoIpsumGenerator } from './../src/dino-ipsum.js';
 
-function isAlphanumericKey(keycode) {
-  return (keycode >= 48) && (keycode <= 90);
-}
+// function isAlphanumericKey(keycode) {
+//   return (keycode >= 48) && (keycode <= 90);
+// }
 
 $(document).ready(function () {
-
-  $("#dinoName").click(function() {
+  let response;
+  let splitResponse;
+  $("#dinoName").click(function () {
     $(".name-button").hide();
     (async () => {
       let dinoIpsumGenerator = new DinoIpsumGenerator();
-      const response = await dinoIpsumGenerator.getDinoNames();
+      response = await dinoIpsumGenerator.getDinoNames();
       getNames(response);
     })();
     $("#guess").show();
-    
 
-  function getNames(response) {
-    if (response) {
-      let splitResponse = response[0][0].split("");
-      for (let i=0; splitResponse.length > i; ++i) {
-        splitResponse.shift();
-        splitResponse.push("__  ");
+    function getNames(response) {
+      if (response) {
+        splitResponse = response[0][0].split("");
+        for (let i = 0; splitResponse.length > i; ++i) {
+          splitResponse.shift();
+          splitResponse.push("__  ");
+        }
+        let hiddenResponse = splitResponse.join("");
+        $('.showName').text(`${hiddenResponse}`);
+      } else {
+        $('.showName').text(`There was an error handling your request.`);
       }
-      let hiddenResponse = splitResponse.join("");
-      $('.showName').text(`${hiddenResponse}`);
-    } else {
-      $('.showName').text(`There was an error handling your request.`);
     }
-  }
 
-  $("#guess").submit(function(event) {
-    event.preventDefault();
-    let inputString = $("#letter").val();
-    let letterArray = [];
-    if (inputString.length !== 1 || inputString.match(/[^a-z]/i)) {
-      $(".error").html("<p>Please only enter one letter.</  p>");
-    } else {
-      $(".error").hide();
-    }      
-    $("#letter").val("");
-  }
+    $("#guess").submit(function (event) {
+      event.preventDefault();
+      let checkArray = response[0][0].toLowerCase().split("");
+      let inputString = $("#letter").val();
+      if (inputString.length !== 1 || inputString.match(/[^a-z]/i)) {
+        $(".error").html("<p>Please only enter one letter.</p>");
+      } else if (!checkArray.includes(inputString)) {
+        return $(".error").html("<p>Incorrect</p>");
+      } else if (checkArray.includes(inputString)) {
+        for (let i = 0; i < checkArray.length; i++) {
+          if (checkArray[i].includes(inputString)) {
+            splitResponse.splice(i, 1, inputString);
+          }
+        }
+        console.log(checkArray);
+        console.log(splitResponse);
+      } else {
+        $(".error").hide();
+      }
+      $("#letter").val("");
+    });
   });
-});
-
-// if (response.includes(inputString)) {
-//   for (i = 0; i < response.length; i++) {
-//     letterArray.push(inputString);
-
-  $(".keyboard-key").on({
-    // Upon mouse-down, make the selectedLetter region immediately visible again
-    // and show the letter that was pressed (which is exactly the button's label).
-    // Also, speak the letter.
-    mousedown: function() {
-        $("#selectedLetter").stop().css('opacity', '1.0').text($(this).text());
-        responsiveVoice.speak($(this).text().toLowerCase(), "UK English Male");
-    },
-
-    // Upon mouse-up, fade away the shown letter.
-    mouseup: function() {
-        $("#selectedLetter").stop().animate({opacity: 0}, 1000);
-    }
-  });
-});
-
-$(document).keydown(function (e) {
-  if (isAlphanumericKey(e.which)) {
-      var myID = "#key-" + String.fromCharCode(e.which);
-      $(myID).trigger("mousedown").addClass('active-style');
-  }
-});
-
-// Emulates a mouseup event on a keyboard-key button.
-$(document).keyup(function (e) {
-  if (isAlphanumericKey(e.which)) {
-      var myID = "#key-" + String.fromCharCode(e.which);
-      $(myID).trigger("mouseup").removeClass('active-style');
-  }
 });
